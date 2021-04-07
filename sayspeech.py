e3b36acfbcd06dff32829fff85a2021e6b3facf8
guillaume <guillaume.jacquin@epitech.eu>
2021-04-07 16:52:49 +0200

    [add] remove useless file

--- sayspeech.py
+++
@@ -1,7 +0,0 @@
- query = myspeech()
-        query = query.lower()
-        print(query)
-        if (query == "retour en arrière"):
-            mess = "Bien, nous revoila dans le menu principal."
-            say_speech(mess, infos)
-            break
